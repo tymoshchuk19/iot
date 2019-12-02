536d3aae99aadf0829a6f4eef01893bd01e4c817
canel <ricardocanela.ns@gmail.com>
2019-12-02 21:54:58 +0000

    minor changes

--- subsilia_2.0/config/env.js
+++ subsilia_2.0/config/env.js
@@ -1,4 +1,4 @@
 module.exports.port = 8819;
 module.exports.mqtt = 'mqtt://quatrobs.hopto.org:1883';
-module.exports.mongoadr = 'mongodb://localhost:27017/campainha'
-//module.exports.mongoadr = 'mongodb+srv://tymoshchuk19:caputadelixo123@quatrobs-q7myi.mongodb.net/test?retryWrites=true&w=majority';
+//module.exports.mongoadr = 'mongodb://localhost:27017/campainha'
+module.exports.mongoadr = 'mongodb+srv://tymoshchuk19:caputadelixo123@quatrobs-q7myi.mongodb.net/test?retryWrites=true&w=majority';
